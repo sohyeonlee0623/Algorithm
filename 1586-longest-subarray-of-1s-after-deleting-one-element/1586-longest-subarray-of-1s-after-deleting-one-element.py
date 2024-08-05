bbfcab143a962588d364655690104c57550b5ffb
Sohyeon_Lee <o3o062357@gmail.com>
2024-08-05 23:03:51 +0900

    Time: 473 ms (45.21%), Space: 17.4 MB (33.62%) - LeetHub

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def longestSubarray(self, nums):
+        left = 0
+        zero_count = 0
+        max_length = 0
+        
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zero_count += 1
+            
+            while zero_count > 1:
+                if nums[left] == 0:
+                    zero_count -= 1
+                left += 1
+            
+            max_length = max(max_length, right - left)
+        
+        return max_length
